ea6a3c728623c885c8cfc6ecc6863e2964602838
joricky91 <jonathanricky91@gmail.com>
2024-01-09 14:56:59 +0700

    Initial Commit

---
+++ Learn by Doing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learn by Doing
+//
+//  Created by Jonathan Ricky Sandjaja on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Jonathan Ricky Sandjaja on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
